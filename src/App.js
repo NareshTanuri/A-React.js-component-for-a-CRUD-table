e80e22415441787be097a45f53499ac1de5f186a
Naresh Tanuri <164649757+NareshTanuri@users.noreply.github.com>
2024-06-07 22:44:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CurdOperationExample from "./Pages/CurdExamplePage";
+
+function App() {
+  return (
+    <>
+      <CurdOperationExample />
+    </>
+  );
+}
+
+export default App;
